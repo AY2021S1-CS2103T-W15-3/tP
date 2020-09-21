87d635bee8ec721ee17ec53049e5c1374790c938
Peh Jun Siang <austin9090@hotmail.com>
2020-09-22 02:10:30 +0800

    Refactor code to adhere to checkstyle

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,11 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Creates a remark with a message.
+     *
+     * @param remark message in the remark.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
